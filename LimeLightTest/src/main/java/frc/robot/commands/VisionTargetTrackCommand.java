64c5b9ea26ede298df1694082abfc215345eee41
Davis <alexdavis@team980.com>
2019-08-05 11:16:53 -0700

    added comments indicating errors in code structure.  also deleted unwanted kill command

--- LimeLightTest/src/main/java/frc/robot/commands/VisionTargetTrackCommand.java
+++ LimeLightTest/src/main/java/frc/robot/commands/VisionTargetTrackCommand.java
@@ -9,11 +9,11 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
-import frc.robot.subsystems.Limelight;
+import frc.robot.subsystems.Limelight;//unnecessary import
 
 
 public class VisionTargetTrackCommand extends Command {
-  private boolean reachedTarget = false;
+  private boolean reachedTarget = false;//all these variables and constants should also be in the subsystem
   private static final double TARGET_AREA = 30;
   private static final double AREA_DEADBAND = 5;
   private static final double FORWARD_SPEED = 0.4;
@@ -24,19 +24,19 @@ public class VisionTargetTrackCommand extends Command {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
     requires(Robot.drivetrain);
-    requires(Robot.limelight);
+    requires(Robot.limelight);//should not need, limelight does not have any methods that would need to be interupted.
   }
 
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
-      Robot.limelight.setPipelineIndex(Limelight.VISION_TARGET_PIPELINE_INDEX);
-  }
+      Robot.limelight.setPipelineIndex(Limelight.VISION_TARGET_PIPELINE_INDEX);//change to Robot.limelight.VISION_TARGET_PIPELINE_INDEX
+  }//you could pass an argument when activating this command indicating which pipeline you want to track
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    if (Robot.limelight.hasTarget()) {
+    if (Robot.limelight.hasTarget()) {//all this code should be in the subsystem. Commands execute methods from the subsystem only
       double area = Robot.limelight.getTrackingArea();
       double xOffset = Robot.limelight.getTrackingX();
 
@@ -69,7 +69,7 @@ public class VisionTargetTrackCommand extends Command {
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return reachedTarget;
+    return reachedTarget;//have a method in the subsystem that returns the reached target variable for this
   }
 
   // Called once after isFinished returns true
